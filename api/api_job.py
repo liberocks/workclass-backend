cef0baf2a18229d40d1d8727e43e8ab49e2b256c
linerocks <tirtadwipa.manunggal@gmail.com>
2021-07-02 14:12:43 +0700

    add pagination and improve filtering

--- api/api_job.py
+++ api/api_job.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponseBadRequest
+from django.db.models import F, Case, When
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from .utils import get_data, get_params, jsonify
 from database.models import Job
+import math
 
 
 @csrf_exempt
@@ -14,28 +16,84 @@ def jobs(req):
     Can be filtered by these parameters:
       1. pageSize: int (amount of jobs to return)
       2. keyword: string (job titles that match this keyword)
+      3. select: string (only return selected attributes)
+      4. page: int (page number)
     """
     params = get_params(req)
 
     keyword = params.get("keyword")
-    page_size = params.get("pageSize", 12)  # default is 12
+    select = params.get("select")
 
+    page_size = int(params.get("page_size", 12))  # default is 12
+    page_size = page_size if page_size > 0 else 12
+
+    page = int(params.get("page", 1))  # default is 1
+    page = page if page > 0 else 1
+
+    # pagination offset
+    offset = (page-1) * page_size
+
+    # jobs object
     jobs = Job.objects
 
-    if keyword:
-        jobs.filter(title__icontains=keyword)
+    # add computed column : salary_median
+    jobs = jobs.annotate(salary_median=(Case(
+        When(salary_period='hourly', then=(195*F('salary_from')+F('salary_to'))/2),  # nopep8 # 195 working our per month , by law
+        When(salary_period='weekly', then=(4*F('salary_from')+F('salary_to'))/2),  # nopep8
+        When(salary_period='yearly', then=(F('salary_from')+F('salary_to'))/(2*12)),  # nopep8
+        When(salary_period='monthly', then=(F('salary_from')+F('salary_to'))/2),  # nopep8
+    )))
+
+    # search, sort, and filter feature
+    order_by_list = []
+    for key, value in params.items():
+        if key == "keyword":
+            jobs = jobs.filter(title__icontains=keyword)
+
+        if key.startswith("job_") or key.startswith("employ_") or key in ["active", "featured"]:
+            if value == "1":
+                jobs = jobs.filter(**{key: True})
+            elif value == "0":
+                jobs = jobs.filter(**{key: False})
+
+        if key in ["salary_period"]:
+            jobs = jobs.filter(**{key: value})
+
+        if key == 'rank_by':
+            if value == "salary_median":
+                order_by_list.append("salary_median")
+            elif value == "-salary_median":
+                order_by_list.append("-salary_median")
 
     # order by descending date
-    jobs = jobs.order_by("-activation_date")
+    jobs = jobs.order_by(*order_by_list, "-activation_date")
 
-    # Trim the amount of jobs to return
-    jobs = jobs.values()[:page_size]
+    # Trim the amount of jobs to return and return only selected columns
+    if select:
+        job_list = jobs[offset:offset+page_size].values(*select.split(","))
+    else:
+        job_list = jobs[offset:offset+page_size].values()
+    job_list = list(job_list)
 
-    return jsonify({"success": True, "jobs": list(jobs)})
+    # metadata
+    jobs_count = jobs.count()
+    total_page = math.ceil(jobs_count/page_size)
+
+    return jsonify({
+        "success": True,
+        "jobs": job_list,
+        "metadata": {
+            "total": jobs_count,
+            "total_page": total_page,
+            "total_item": len(job_list),
+            "page": page,
+            "page_size": page_size,
+        }
+    })
 
 
-@csrf_exempt
-@require_http_methods(["GET", "POST"])
+@ csrf_exempt
+@ require_http_methods(["GET", "POST"])
 def job(req):
     """
     API handler for /job
